1b567fb4c208ef3cfa000ac1eb75b33824127f59
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-18 23:12:49 -0300

    Definindo o componente principal App.

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="main-container">
+    <Header/>
+    <router-view/>
+  </main>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name:"App",
+  components:{
+    Header
+  }
+}
+</script>
+
