584afca62bba77a7937e381337b72decc547e9ee
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-18 23:58:21 -0300

    Criando o componente Header.

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header-container">
+        <div class="center">
+            <h1 class="header-title"><i class="fa-solid fa-circle-check"></i> Pomofocus</h1>
+            <div class="menu-container">
+                <div class="menu-button"><a href=""><i class="fa-solid fa-chart-column"></i> Report</a></div>
+                <div class="menu-button"><a href=""><i class="fa-solid fa-gear"></i> Setting</a></div>
+                <div class="menu-button"><a href=""><i class="fa-solid fa-circle-user"></i> Login</a></div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
